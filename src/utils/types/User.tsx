ab969d7010094a142560b30fb918c24867d36007
Trần Chí Mỹ <102143951+MyTran2508@users.noreply.github.com>
2022-11-26 22:33:10 +0700

    add user type

---
+++ src/utils/types/User.tsx
@@ -0,0 +1,35 @@
+// import Specialization from './Specialization';
+// import UserRole from './UserRole';
+// * My day 5
+// export interface User {
+//   id: string;
+//   name: string;
+//   email: string;
+//   role: UserRole;
+//   specialization?: Specialization;
+// }
+
+// export interface Educator extends User {
+//   role: UserRole.EDUCATOR;
+//   specialization: Specialization;
+// }
+
+// export type BookmarkTopics = {
+//   [topicId: string]: string[];
+// };
+
+// export interface Student extends User {
+//   role: UserRole.STUDENT;
+//   bookmarkedTopics: BookmarkTopics;
+//   appliedTopics: string[];
+// }
+
+// export interface Admin extends User {
+//   role: UserRole.ADMIN;
+// }
+
+// export interface HeadOfDepartment extends User {
+//   role: UserRole.HEAD_OF_DEPARTMENT;
+// }
+
+// export default User;
---
+++ src/utils/types/UserRole.tsx
@@ -0,0 +1,9 @@
+enum UserRole {
+  ADMIN = 'admin',
+  EDUCATOR = 'educator',
+  STUDENT = 'student',
+  HEAD_OF_DEPARTMENT = 'head of department',
+  GUEST = 'guest',
+}
+
+export default UserRole;
