80c768306e506ebf0508b9a1371e63d962746dbb
Thanh Nhan <hctxnhan@gmail.com>
2022-12-01 23:31:30 +0700

    day 6: Fix slice

--- src/redux/features/topicDetailSlice.tsx
+++ src/redux/features/topicDetailSlice.tsx
@@ -1,48 +1,46 @@
-// * Loc day 6
+import { createSlice } from '@reduxjs/toolkit'
+import { RootState } from '../store'
 
-// import { createSlice } from '@reduxjs/toolkit';
-// import { RootState } from '../store';
+type TopicDetailState = {
+  open: boolean
+  topicId: string
+  openCreatePeriod: boolean
+}
 
-// type TopicDetailState = {
-//   open: boolean;
-//   topicId: string;
-//   openCreatePeriod: boolean;
-// };
+const initialState: TopicDetailState = {
+  open: false,
+  openCreatePeriod: false,
+  topicId: '',
+}
 
-// const initialState: TopicDetailState = {
-//   open: false,
-//   openCreatePeriod: false,
-//   topicId: '',
-// };
+const topicDetailSlice = createSlice({
+  name: 'topicDetail',
+  initialState,
+  reducers: {
+    openTopicDetail(state, action) {
+      state.open = true
+      state.topicId = action.payload
+    },
+    closeTopicDetail(state) {
+      state.open = false
+    },
+    openCreatePeriod(state) {
+      state.openCreatePeriod = true
+    },
+    closeCreatePeriod(state) {
+      state.openCreatePeriod = false
+    },
+  },
+})
 
-// const topicDetailSlice = createSlice({
-//   name: 'topicDetail',
-//   initialState,
-//   reducers: {
-//     openTopicDetail(state, action) {
-//       state.open = true;
-//       state.topicId = action.payload;
-//     },
-//     closeTopicDetail(state) {
-//       state.open = false;
-//     },
-//     openCreatePeriod(state) {
-//       state.openCreatePeriod = true;
-//     },
-//     closeCreatePeriod(state) {
-//       state.openCreatePeriod = false;
-//     },
-//   },
-// });
-
-// export const {
-//   openTopicDetail,
-//   closeTopicDetail,
-//   openCreatePeriod,
-//   closeCreatePeriod,
-// } = topicDetailSlice.actions;
-// export default topicDetailSlice.reducer;
-// export const selectTopicOpen = (state: RootState) => state.topicDetail.open;
-// export const selectTopicId = (state: RootState) => state.topicDetail.topicId;
-// export const selectOpenCreatePeriod = (state: RootState) =>
-//   state.topicDetail.openCreatePeriod;
+export const {
+  openTopicDetail,
+  closeTopicDetail,
+  openCreatePeriod,
+  closeCreatePeriod,
+} = topicDetailSlice.actions
+export default topicDetailSlice.reducer
+export const selectTopicOpen = (state: RootState) => state.topicDetail.open
+export const selectTopicId = (state: RootState) => state.topicDetail.topicId
+export const selectOpenCreatePeriod = (state: RootState) =>
+  state.topicDetail.openCreatePeriod
