77ffa8cf68585ae390fa0d058e230b7da79f4f77
Trần Chí Mỹ <102143951+MyTran2508@users.noreply.github.com>
2022-12-02 08:41:04 +0700

    add no authenticated

---
+++ src/redux/features/NoAuthenticated.tsx
@@ -0,0 +1,22 @@
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+import useCheckLogin from '../hooks/useCheckLogin'
+
+
+function NoAuthenticated() {
+  const navigate = useNavigate();
+  const isLoggedIn = useCheckLogin();
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      navigate('/');
+    }, 3000);
+    return () => {
+      clearTimeout(id);
+    };
+  }, []);
+
+  return <div>Lỗi: Chưa đăng nhập vào hệ thống! Đang quay về trang chủ!</div>;
+}
+
+export default NoAuthenticated;
