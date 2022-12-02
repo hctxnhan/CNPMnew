81d01bedf6b59b29fd89ee240860898f88d00cfb
Thanh Nhan <hctxnhan@gmail.com>
2022-12-02 23:37:39 +0700

    day 7 : Add topics tabs

---
+++ src/pages/FindTopics.tsx
@@ -0,0 +1,23 @@
+import EmptyTopicList from '../components/EmptyTopicList'
+import Search from '../components/Search'
+import TabPane from '../components/TabPane'
+import Tabs from '../components/Tabs'
+import TopicList from '../components/TopicList'
+import { selectPeriods } from '../redux/features/topicSlice'
+import { useAppSelector } from '../redux/store'
+
+function FindTopics() {
+  const periods = useAppSelector(selectPeriods)
+  const allPeriods = periods.map((period) => period.name)
+  const tabsPanel = periods.map((period) => {
+    if (period.topics.length === 0) return <EmptyTopicList />
+    return (
+      <TabPane>
+        <TopicList topics={period.topics} period={period.id} />
+      </TabPane>
+    )
+  })
+  return <Tabs tabs={allPeriods} data={tabsPanel} />
+}
+
+export default FindTopics
