381ea0a5f26f79dab28cd6edb0c13470d9ca3e78
Trần Chí Mỹ <102143951+MyTran2508@users.noreply.github.com>
2022-12-08 22:56:02 +0700

    add function useCheckactivetopic

---
+++ src/hooks/useCheckActiveTopic.tsx
@@ -0,0 +1,20 @@
+import {
+  selectPeriodById,
+  selectTopicById,
+  selectTopicPeriodId,
+} from '../redux/features/topicSlice';
+import { useAppSelector } from '../redux/store';
+import checkIfPeriodActive from '../utils/functions/CheckIfPeriodACtive';
+
+function useCheckActiveTopic(topicId: string) {
+  const periodId = useAppSelector((state) =>
+    selectTopicPeriodId(state, topicId)
+  );
+
+  const period = useAppSelector((state) => selectPeriodById(state, periodId));
+  if (!period) return false;
+
+  return checkIfPeriodActive(period);
+}
+
+export default useCheckActiveTopic;
