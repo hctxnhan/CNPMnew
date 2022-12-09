247143e84ea7a63bf491adb2f664d9f71ff819a7
Baolocace111 <baolocace111@gmail.com>
2022-12-09 09:30:44 +0700

    Add delete topic function for Bookmark page

---
+++ src/components/TopicList.tsx
@@ -0,0 +1,21 @@
+import Topic from '../utils/types/Topic';
+import TopicComponent from './TopicComponent';
+import { educators } from '../firebase/userMock';
+import TabPane from './TabPane';
+
+type TopicListProps = {
+  topics: Topic[];
+  period: string;
+};
+
+function TopicList({ topics, period }: TopicListProps) {
+  return (
+    <div className='grid grid-cols-3 gap-4 lg:grid-cols-2 md:grid-cols-1'>
+      {topics.map((topic) => (
+        <TopicComponent key={topic.id} topic={topic} period={period} />
+      ))}
+    </div>
+  );
+}
+
+export default TopicList;
